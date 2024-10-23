d4b1ac0bd8d033d33ddd2e4af4df4dbb325587da
Mary8ky <jhojanagamboa@gmail.com>
2024-10-23 07:33:27 -0500

    creación tiendas react

---
+++ 1-intro-react/1-tienda-de-naranjas/src/App.jsx
@@ -0,0 +1,19 @@
+
+function App() {
+ 
+
+  return(
+    <div>
+      <header>
+        <nav>menu de navegacion</nav>
+      </header>
+      <section>seccion promos 🍊</section>
+      <section>seccion tienda🥤</section>
+      <section>pie de pagina</section>
+    </div>
+    )
+   
+  }
+
+
+export default App
